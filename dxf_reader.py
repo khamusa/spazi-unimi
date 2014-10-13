ea290fb97cfafa65538a771367ff05e1b6107851
Samuel Brandão <gb.samuel@gmail.com>
2014-10-13 13:54:16 +0200

    Dxf Reader adesso crea lista di oggetti di tipo Room e RoomText. Aggiunti test di creazione di Point a partire da tuple con tre elementi (per compatibilità nella creazione di RoomText a partire da MText)

--- dxf_reader.py
+++ dxf_reader.py
@@ -8,7 +8,11 @@
 
 import dxfgrabber
 import sys
-from dxfgrabber.entities import *
+import svgwrite
+from room import Room
+from room_text import RoomText
+from point import Point
+from dxfgrabber.entities import LWPolyline, Polyline, MText, Text
 
 class DxfReader():
    # Todo: extract to external config file
@@ -20,16 +24,19 @@ class DxfReader():
       self._grabber = dxfgrabber.readfile(self._filename)
 
       self.rooms = [
-            ent for ent in self._grabber.entities \
+            Room(ent.points) for ent in self._grabber.entities \
             if type(ent) in [LWPolyline, Polyline] and ent.is_closed and ent.layer in self.valid_poly_layers
-            ]
-
-      self.texts = (ent for ent in self._grabber.entities if type(ent) in [MText, Text])
+            
+      # TODO: Controllare se ci sono casi di Mtext che restituiscano una lista
+      # di testi alla chiamata di plain_text()
+      self.texts = (RoomText(ent.plain_text(), Point(ent.insert) )            \
+            for ent in self._grabber.entities if type(ent) in [MText, Text]
+            )
 
 
 
 if __name__ == '__main__':
-   fname = (len(sys.argv) > 1) and sys.argv[1] or "../src/stanza_singola.DXF" 
+   fname = (len(sys.argv) > 1) and sys.argv[1] or "assets/stanza_singola.DXF" 
    dx = DxfReader(fname)
    print(dx.rooms)
    print(list(dx.texts))
--- test/point_test.py
+++ test/point_test.py
@@ -6,3 +6,6 @@ class PointTest(unittest.TestCase):
       # Test rounding function actually being applied
       self.assertTrue( 3.333 == Point(3.33333, 1).x )
       self.assertTrue( 3.333 == Point(1, 3.3333333).y )
+
+      self.assertTrue( 10 == Point( (10, 20, 30) ).x )
+      self.assertTrue( 20 == Point( (10, 20, 30) ).y )
