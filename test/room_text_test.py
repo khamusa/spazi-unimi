29544e916cc5b536b683e378fc3d1dcd2b33eb48
Samuel Brandão <gb.samuel@gmail.com>
2014-10-16 20:56:47 +0200

    Aggiunta di test dell'immutabilita' di RoomText

--- test/room_text_test.py
+++ test/room_text_test.py
@@ -7,12 +7,16 @@ class RoomTextTest(unittest.TestCase):
       t  = RoomText( "ABC", Point(20, 20) )
       t2 = t.traslated(10, 20)
 
+      # traslated must retur a new object
+      self.assertFalse(t2 is t)
       self.assertTrue( t2.anchor_point.x==30 )
       self.assertTrue( t2.anchor_point.y==40 )
 
       t2 = t.traslated(-10, -20)
 
+      # traslated must retur a new object
+      self.assertFalse(t2 is t)
       self.assertTrue( t2.anchor_point.x==10 )
       self.assertTrue( t2.anchor_point.y==0 )
 
-   
+
