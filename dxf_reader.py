cdbc7ca67991fdbac95abf5a8ed6a95d72242382
Samuel Brandão <gb.samuel@gmail.com>
2014-10-20 12:01:56 +0200

    Fissando qualche bug e scalando l'SVG generato

--- dxf_reader.py
+++ dxf_reader.py
@@ -25,12 +25,12 @@ class DxfReader():
 
       self.rooms = [
             Room(ent.points).reflected_y() for ent in self._grabber.entities \
-            if type(ent) in [LWPolyline, Polyline] and ent.is_closed and ent.layer in self.valid_poly_layers]
+            if type(ent) in [LWPolyline, Polyline] and ent.layer in self.valid_poly_layers]
 
       # TODO: Controllare se ci sono casi di Mtext che restituiscano una lista
       # di testi alla chiamata di plain_text()
       texts = (RoomText(ent.plain_text(), Point(ent.insert) ).reflected_y()  \
-            for ent in self._grabber.entities if type(ent) in [MText, Text]
+            for ent in self._grabber.entities if type(ent) in [MText, Text] and ent.layer in self.valid_text_layers
             )
 
       for t in texts:
@@ -44,23 +44,29 @@ if __name__ == '__main__':
    fname = (len(sys.argv) > 1) and sys.argv[1] or "assets/dxf/stanza_singola.dxf"
 
    import time
+   import random
    time_s = time.time()
    dx = DxfReader(fname)
 
-   minimum_x = 999999
-   minimum_y = 999999
+   minimum_x = float('+inf')
+   minimum_y = float('+inf')
    for r in dx.rooms:
+      r.scale(.4)
       minimum_x = min(minimum_x, r.top_left_most_point().x)
       minimum_y = min(minimum_y, r.top_left_most_point().y)
 
    svg = svgwrite.Drawing()
 
    for r in dx.rooms:
-      for p in r.points:
-         p.traslate(-minimum_x, -minimum_y)
+      r.traslate(-minimum_x, -minimum_y)
 
-      points   = svg.polyline( ( (p.x, p.y) for p in r.points ), fill="#f00",stroke="#666")
+      color = "rgb({}, {}, {})".format(int(random.random()*200), int(random.random()*200), int(random.random()*200))
+      points   = svg.polyline( ( (p.x, p.y) for p in r.points ), fill=color,stroke="#666")
+
       svg.add(points)
+
+      for t in r.texts:
+         svg.add(svg.text(t.text, t.anchor_point))
 
    svg.filename = "assets/test.svg"
    svg.save()
