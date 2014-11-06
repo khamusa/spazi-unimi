fbcb53258bb7faa5bf1a3fabec8481235036e217
paoloventuri <paolo.venturi@studenti.unimi.it>
2014-11-06 15:38:54 +0100

    Aggiunti test per la clean di una collezione e per l'inserimento di più room_category

--- test/db_test.py
+++ test/db_test.py
@@ -18,3 +18,16 @@ class DbTest(unittest.TestCase):
       args = { "cat_id" : "123" , "cat_name" : "room" }
       self.pm.insert_room_category( args )
       self.db["room_categories"].insert.assert_called_once_with( args )
+
+   def test_insertroomcategories(self):
+      args = [ "first", "second" ]
+      self.pm.insert_room_category = MagicMock()
+      self.pm.clean_collection = MagicMock()
+      self.pm.insert_room_categories( args )
+      self.pm.clean_collection.assert_called_once_with("room_categories")
+      self.pm.insert_room_category.assert_any_call("first")
+      self.pm.insert_room_category.assert_any_call("second")
+
+   def test_cleancollection(self):
+      self.pm.clean_collection("room_categories")
+      self.db["room_categories"].drop.assert_called_once_with()
