4b1995f6d93f4f192c5f3c650838a3114fec7725
paoloventuri <paolo.venturi@studenti.unimi.it>
2014-11-06 17:12:43 +0100

    Aggiunto controllo del CSV header con eccezione in caso di header non riconosciuto + test Aggiunta chiamata del metodo di update per room_category + test

--- csv_data_updater.py
+++ csv_data_updater.py
@@ -1,12 +1,27 @@
 from csv_reader import CSVReader
 
-class CSVDataUpdater:
-   def __init__(self, headers_dict):
-      self._valid_headers = { k : set(v) for k, v in headers_dict.items() }
+class InvalidCSVHeaderError(RuntimeError):
+   pass
 
-   def perform_update(self, filename):
-      reader   = CSVReader(filename)
+class CSVDataUpdater:
+   def __init__(self, headers_dict, persistence_manager):
+      self._valid_headers = { k : set(v) for k, v in headers_dict.items() }
+      self._pm = persistence_manager
+
+   def perform_update(self, filename, reader_class = CSVReader):
+      reader   = reader_class(filename)
       csv_type = self.infer_csv_from_header(reader.header)
+
+      if csv_type == None:
+         raise InvalidCSVHeaderError("L'intestazione del file CSV non è valida")
+
+      {
+         "rooms"           : lambda s: None,
+         "buildings"       : lambda s: None,
+         "room_categories" : self.update_room_categories
+
+      }[csv_type](reader.content)
+
 
    def infer_csv_from_header(self, effective_header):
       is_in_effective_header = lambda s: s in is_in_effective_header
@@ -16,3 +31,6 @@ class CSVDataUpdater:
             return valid_key
 
       return None
+
+   def update_room_categories(self, categories):
+      self._pm.insert_room_categories(categories)
--- test/csv_data_updater_test.py
+++ test/csv_data_updater_test.py
@@ -1,17 +1,18 @@
 import unittest
-from csv_data_updater import CSVDataUpdater
+from csv_data_updater import CSVDataUpdater, InvalidCSVHeaderError
+from mock import MagicMock
 
 class CSVDataUpdaterTest(unittest.TestCase):
 
    def setUp(self):
-      self.reader = CSVDataUpdater({
+      self.csv_updater = CSVDataUpdater({
             "a": ["1", "2", "3"],
             "b": ["4", "5", "6"]
-         })
+         }, MagicMock())
 
 
    def test_header_inference(self):
-      infer = self.reader.infer_csv_from_header
+      infer = self.csv_updater.infer_csv_from_header
 
       self.assertEqual("b", infer({"4", "5", "6"}) )
       self.assertEqual("b", infer({"4", "2", "5", "6"}) )
@@ -35,3 +36,22 @@ class CSVDataUpdaterTest(unittest.TestCase):
       self.assertEqual(None, infer({"4", "b", "3", "1"}) )
       self.assertEqual(None, infer({"3", "1", "8", "c"}) )
       self.assertEqual(None, infer({"4", "5", "3", "HA", "pippo", "pluto"}) )
+
+
+   def test_update_categories_calls_persistence_manager_correctly(self):
+      cat = ["first", "second"]
+      self.csv_updater.update_room_categories(cat)
+      self.csv_updater._pm.insert_room_categories.assert_called_once_with(cat)
+
+   def test_perform_update_calls_correct_update_method_for_room_categories(self):
+      readerMock = MagicMock(return_value = MagicMock(content = "pippo"))
+      self.csv_updater.infer_csv_from_header = MagicMock(return_value = "room_categories")
+      self.csv_updater.update_room_categories = MagicMock()
+      self.csv_updater.perform_update("Test.csv", reader_class = readerMock)
+      self.csv_updater.update_room_categories.assert_called_once_with("pippo")
+
+   def test_perform_update_raises_exception_on_unknown_csv_type(self):
+      readerMock = MagicMock()
+      self.csv_updater.infer_csv_from_header = MagicMock(return_value = None)
+      with self.assertRaises(InvalidCSVHeaderError):
+         self.csv_updater.perform_update("Test.csv", reader_class = readerMock)
