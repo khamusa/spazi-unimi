2ce0654bf7aea62b2e38652aad32dd654888ecc2
Samuel Brandão <gb.samuel@gmail.com>
2014-11-24 01:07:22 +0100

    run_test.py permette di specificare il nome del file di test da eseguire, invece di eseguirne tutti i test

--- run_tests.py
+++ run_tests.py
@@ -4,11 +4,19 @@ import sys
 from os import listdir
 import re
 
-for filename in listdir('test'):
+if(len(sys.argv) == 1):
+   files_list = listdir('test')
+else:
+   files_list = sys.argv[1:]
+   sys.argv = sys.argv[0:1]
+
+for filename in files_list:
+   print(filename)
    if re.match(".+test\.py" , filename):
       s = __import__("test."+filename.rsplit(".py")[0], globals(), locals(), ['*'], 0)
       potential_class_names = [local for local in dir(s) if re.match("[A-Z][^_]+", local) ]
       for classname in potential_class_names:
          setattr(sys.modules[__name__], classname, getattr(s, classname))
+
 
 unittest.main(exit=False, verbosity = 2)
