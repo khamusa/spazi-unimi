bea1b45acbce559a2dd074c9f3bcb03d2555e537
Samuel Brandão <gb.samuel@gmail.com>
2014-11-25 16:31:05 +0100

    Possibilità di specificare livello di verbosity della classe logger

--- utils/logger.py
+++ utils/logger.py
@@ -7,17 +7,27 @@ class Logger():
    ERR     = '\033[91m' # red
    ENDC    = '\033[0m'  # reset color
 
+   VERBOSITY_ALL        = 3
+   VERBOSITY_INFO       = 3
+   VERBOSITY_WARNINGS   = 2
+   VERBOSITY_FATAL      = 1
+   verbosity = VERBOSITY_ALL
+
    def success(*texts):
-      print(Logger._prefix("OK!", Logger.OK), *texts, file=sys.stderr)
-
-   def warning(*texts):
-      print(Logger._prefix("WARN", Logger.WARN), *texts, file=sys.stderr)
-
-   def error(*texts):
-      print(Logger._prefix("ERR", Logger.ERR), *texts, file=sys.stderr)
+      if(Logger.verbosity >= Logger.VERBOSITY_INFO):
+         print(Logger._prefix(" OK ", Logger.OK), *texts, file=sys.stderr)
 
    def info(*texts):
-      print(Logger._prefix("INFO", Logger.INFO), *texts)
+      if(Logger.verbosity >= Logger.VERBOSITY_INFO):
+         print(Logger._prefix("INFO", Logger.INFO), *texts)
+
+   def warning(*texts):
+      if(Logger.verbosity >= Logger.VERBOSITY_WARNINGS):
+         print(Logger._prefix("WARN", Logger.WARN), *texts, file=sys.stderr)
+
+   def error(*texts):
+      if(Logger.verbosity >= Logger.VERBOSITY_FATAL):
+         print(Logger._prefix(" ERR", Logger.ERR), *texts, file=sys.stderr)
 
    def _prefix(key, color):
       if (os.fstat(0) == os.fstat(1)):
