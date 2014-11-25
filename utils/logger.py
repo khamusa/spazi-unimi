43a8657bd330cc83976490b1ab47cac372762273
Samuel Brandão <gb.samuel@gmail.com>
2014-11-25 16:24:55 +0100

    Ia classe logger aggiunge i colori ai messaggi, se e solo se stdout non e' redirezionato. Inoltre adesso i suoi metodi accettano varargs, e e' stato aggiunto il metodo success, un correlato di info ma che ha lo scopo di informare che operazioni sono andate a buon fine.

--- utils/logger.py
+++ utils/logger.py
@@ -1,12 +1,26 @@
-import sys
+import sys, os
 
 class Logger():
+   INFO    = '\033[94m' # blue
+   OK      = '\033[92m' # green
+   WARN    = '\033[93m' # yellow
+   ERR     = '\033[91m' # red
+   ENDC    = '\033[0m'  # reset color
 
-   def warning(text):
-         print("[WARN] :", text,file=sys.stderr)
+   def success(*texts):
+      print(Logger._prefix("OK!", Logger.OK), *texts, file=sys.stderr)
 
-   def error(text):
-      print(" [ERR] :",text,file=sys.stderr)
+   def warning(*texts):
+      print(Logger._prefix("WARN", Logger.WARN), *texts, file=sys.stderr)
 
-   def info(text):
-      print("[INFO] :",text)
+   def error(*texts):
+      print(Logger._prefix("ERR", Logger.ERR), *texts, file=sys.stderr)
+
+   def info(*texts):
+      print(Logger._prefix("INFO", Logger.INFO), *texts)
+
+   def _prefix(key, color):
+      if (os.fstat(0) == os.fstat(1)):
+         return "["+color+key+Logger.ENDC+"] :"
+      else:
+         return "["+key+"] :"
