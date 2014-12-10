5031ed03da806bcbab2a87fa7211848ca4777526
Samuel Brandão <gb.samuel@gmail.com>
2014-12-10 15:56:05 +0100

    Rimozione di metodi non piu' utilizzati su MongoDbPersistenceManager

--- persistence/db/mongo_db_persistence_manager.py
+++ persistence/db/mongo_db_persistence_manager.py
@@ -2,13 +2,6 @@ from . import DB;
 
 class MongoDBPersistenceManager:
 
-   ROOM_CATEGORIES = "room_categories"
-   BUILDINGS = "buildings"
-   BID_TO_LBID = "bid_to_lbid"
-   ROOMS = "rooms"
-
-   _valid_collections = [ROOM_CATEGORIES, BUILDINGS, BID_TO_LBID, ROOMS]
-
    def __init__(self, config = None, db = None):
       if config is not None:
          db_cfg = config["db"]
@@ -19,31 +12,8 @@ class MongoDBPersistenceManager:
    def clean_collection(self,collection_name):
       self.db[collection_name].drop()
 
-   def insert_room_category(self, category):
-      self.db[self.ROOM_CATEGORIES].insert(category)
+   def save(self, collection_name, value):
+      self.db[collection_name].save(value)
 
-   def insert_room_categories(self,categories):
-      for c in categories:
-         self.insert_room_category(c)
-
-   def insert_building(self,building):
-      self.db[self.BUILDINGS].insert(building)
-
-   def insert_buildings(self,buildings):
-      for b in buildings:
-         self.insert_building(b)
-
-   def insert_room(self,room):
-      self.db[self.ROOMS].insert(room)
-
-   def insert_rooms(self,rooms):
-      for r in rooms:
-         self.insert_room(r)
-
-   def insert_bid_lookup_table(self,buildings):
-      # create indexes
-      self.db[self.BID_TO_LBID].ensure_index("l_b_id")
-      self.db[self.BID_TO_LBID].ensure_index("b_id")
-
-      for b in buildings:
-         self.db[self.BID_TO_LBID].insert({ "l_b_id":b["l_b_id"] , "b_id" : b["b_id"] })
+   def get_collection(self, collection_name):
+      return self.db[collection_name]
--- test/db_test.py
+++ test/db_test.py
@@ -13,61 +13,6 @@ class DbTest(unittest.TestCase):
       config = ConfigManager("config/general.json")["db"]
       db = DB( config["url"], config["port"], config["db_name"])
 
-   def test_roomcategory_save(self):
-      args = { "cat_id" : "123" , "cat_name" : "room" }
-      self.pm.insert_room_category( args )
-      self.db[self.pm.ROOM_CATEGORIES].insert.assert_called_once_with( args )
-
-   def test_insertroomcategories(self):
-      args = [ "first", "second" ]
-      self.pm.insert_room_category = MagicMock()
-      self.pm.insert_room_categories( args )
-      self.pm.insert_room_category.assert_any_call("first")
-      self.pm.insert_room_category.assert_any_call("second")
-
    def test_cleancollection(self):
-      self.pm.clean_collection(self.pm.ROOM_CATEGORIES)
-      self.db[self.pm.ROOM_CATEGORIES].drop.assert_called_once_with()
-
-
-   def test_room_save(self):
-      args = {
-               "b_id"      : "213",
-               "address"   : "via Senato",
-               "l_floor"   : "first",
-               "r_id"      : "31331",
-               "type_name" : "aula",
-               "room_name" : "1231231",
-               "capacity"  : "1"
-            }
-
-      self.pm.insert_room(args)
-      self.db[self.pm.ROOMS].insert.assert_called_once_with(args)
-
-   def test_insertrooms(self):
-      args = ["first","second"]
-      self.pm.insert_room  = MagicMock()
-      self.pm.insert_rooms(args)
-      self.pm.insert_room.assert_any_call("first")
-      self.pm.insert_room.assert_any_call("second")
-
-
-   def test_building_save(self):
-      args = {
-            "l_b_id"    : "123",
-            "b_id"      : "1234",
-            "address"   : "via Senato, Milano",
-            "lat"       : "12341.12231",
-            "lon"       : "123.23"
-      }
-
-      self.pm.insert_building(args)
-      self.db[self.pm.BUILDINGS].insert.assert_called_once_with(args)
-
-
-   def test_insertbuildings(self):
-      args = ["first","second"]
-      self.pm.insert_building = MagicMock()
-      self.pm.insert_buildings(args)
-      self.pm.insert_building.assert_any_call("first")
-      self.pm.insert_building.assert_any_call("second")
+      self.pm.clean_collection("building")
+      self.db["building"].drop.assert_called_once_with()
