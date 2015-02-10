90da1fb71c7ab32a9a37f6128e0f5c3576bd5297
paoloventuri <paolo.venturi@studenti.unimi.it>
2015-02-10 13:22:01 +0100

    Modificato il salvataggio delle stanze nel DB in modo che non contengano l'attributo b_id

--- tasks/data_updater.py
+++ tasks/data_updater.py
@@ -27,15 +27,28 @@ class DataUpdater():
    def update_rooms(self,rooms):
          namespace = self.get_namespace()
          floor_key = self.get_floor_key()
+
+         # ordiniamo le stanze per edificio e per piano in modo da velocizzare l'algoritmo
          keyfunc = lambda s: (s["b_id"], s[floor_key])
          rooms.sort(key = keyfunc)
          rooms = itertools.groupby(rooms, key = keyfunc)
          building = None
 
+         # remove the attribute b_id from the room
+         def remove_b_id(room):
+            del room["b_id"]
+            return room
+
+
          for ((b_id, floor), floor_rooms) in rooms:
+
+            # remove the attribute b_id from the rooms
+            floor_rooms = map(remove_b_id, floor_rooms)
+
             if not self._is_valid_b_id(b_id):
                Logger.warning("Invalid building ID: \"{}\"".format(b_id))
                continue
+
             if not building or building.attr("b_id") != b_id:
                building and building.save()
                building = Building.find_or_create_by_id(b_id)
