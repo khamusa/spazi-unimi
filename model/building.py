d909277c58e06f34ed20129b48d625618236f176
Samuel Brandão <gb.samuel@gmail.com>
2015-02-11 16:40:17 +0100

    Building garantisce l'ordinamento dei piani sotto "dxf", "edilizia" e anche "easyrooms" utilizzando i callback di ORMModel

--- model/building.py
+++ model/building.py
@@ -5,4 +5,12 @@ class Building(ORMModel):
    def __init__(self, new_attrs = None):
       super().__init__(new_attrs, external_id = "b_id")
 
+   def ensure_floors_sorting(self):
+      # Ordina l'array "floors" all'interno dei dizionari dxf, edilizia e easyroom
+      for k in ["dxf", "easyroom", "edilizia"]:
+         namespace = self.attr(k)
 
+         if namespace and "floors" in namespace:
+            namespace["floors"].sort(key=lambda s: float(s["f_id"]))
+
+Building.before_save("ensure_floors_sorting")
