52368690e799c92965ca0099e506c29a06190a0d
paoloventuri <paolo.venturi@studenti.unimi.it>
2015-02-26 11:39:23 +0100

    Modificata l'espressione regolare per matchare più b_id dai nomi dei DXF

--- tasks/dxf/dxf_reader.py
+++ tasks/dxf/dxf_reader.py
@@ -120,7 +120,7 @@ class DxfReader():
       """
 
       b_id  = None
-      rm    = re.match("(\w+)_(\w+)\.dxf", basename, re.I)
+      rm    = re.match("(\d+)_([a-zA-Z0-9]+).*\.dxf", basename, re.I)
 
       if rm:
          b_id = rm.group(1)
