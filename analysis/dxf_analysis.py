503390cf858a2391bd201aee8331433b073d56f3
paoloventuri <paolo.venturi@studenti.unimi.it>
2015-03-16 14:38:29 +0100

    Aggiunta raccolata delle statistiche generali sui dati da DXF.

--- analysis/dxf_analysis.py
+++ analysis/dxf_analysis.py
@@ -1,6 +1,7 @@
 from collections import Counter
 
 class DXFAnalysis():
+   general_count = Counter()
 
    @classmethod
    def analyse_dxf_info(klass, building):
@@ -39,18 +40,34 @@ class DXFAnalysis():
       """
       count       = Counter()
       identified  = set(floor.get("rooms", {}).keys())
-      count.update({"identified" : len(identified)})
-      count.update({"walls"      : len(floor.get("walls", []))})
-      count.update({"windows"    : len(floor.get("windows", []))})
+      count.update({"identified_rooms" : len(identified)})
+      count.update({"walls"            : len(floor.get("walls", []))})
+      count.update({"windows"          : len(floor.get("windows", []))})
 
       for room in floor.get("unidentified_rooms", []):
          if "cat_name" in room:
-            count.update({"categorized" : 1})
+            count.update({"categorized_rooms" : 1})
          else:
-            count.update({"no_info" : 1})
+            count.update({"no_info_rooms" : 1})
 
-      total = count["identified"] + count["categorized"] + count["no_info"]
-      count.update({"total" : total})
+      total =  count["identified_rooms"] + count["categorized_rooms"] + count["no_info_rooms"]
+
+      count.update({"total_rooms" : total})
       f_id  = floor.get("f_id", "")
 
+      klass._update_general_stats(count)
+
       return (f_id, count, {"identified" : identified})
+
+   @classmethod
+   def _update_general_stats(klass, floor_count):
+      klass.general_count.update({
+         "dxf.total_rooms"       : floor_count["total_rooms"],
+         "dxf.identified_rooms"  : floor_count["identified_rooms"],
+         "dxf.categorized_rooms" : floor_count["categorized_rooms"],
+         "dxf.no_info_rooms"     : floor_count["no_info_rooms"],
+         "dxf.total_floors"      : 1
+      })
+
+      if floor_count["identified_rooms"]:
+         klass.general_count.update({"dxf.matched_floors" : 1})
