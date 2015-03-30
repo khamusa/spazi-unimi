39b1d84b734a51288da9fabd3b9d6993df48a853
Diego Costantino <diegocstn@gmail.com>
2015-03-30 15:04:11 +0200

    ricerca buildings per coordinate

--- api/api.py
+++ api/api.py
@@ -1,4 +1,4 @@
-from flask                 import Flask, jsonify,abort
+from flask                 import Flask, jsonify,abort,request
 from model.building_view   import BuildingView
 from bson.json_util        import dumps
 from utils                 import ConfigManager
@@ -9,6 +9,9 @@ app                     = Flask(__name__)
 app.app_config          = ConfigManager("config/general.json")
 app.persistence         = MongoDBPersistenceManager(app.app_config)
 ODMModel.set_pm( app.persistence )
+
+# radius used with GeoSpatial Query (meters)
+app.radius              = 2000
 
 
 ###########
@@ -32,4 +35,23 @@ def get_building_by_id(b_id):
    if not building:
        abort(404)
    return jsonify(building)
+
+@app.route('/api/v1.0/buildings/near/<float:lat>/<float:lng>',methods=['GET'])
+def get_buildings_near_position(lat,lng):
+   r = request.args.get('radius') or app.radius
+   if (not lat) or (not lng):
+      abort(404)
+
+   geo_json_point = {
+      "$geometry" : {
+         "type"         : "Point",
+         "coordinates"  : [ lng , lat ] },
+         "$maxDistance" : int(r)
+   }
+
+   buildings   = list(app.buildings.find({ "coordinates" : { "$near" : geo_json_point } }))
+
+   return jsonify({ 'buildings': buildings })
+
+
 
