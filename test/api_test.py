f415ba5f601fc95c44a4f19ea704d465886d9349
Diego Costantino <diegocstn@gmail.com>
2015-08-05 20:42:35 +0200

    fix api tests after refactoring of package

--- test/api_test.py
+++ test/api_test.py
@@ -1,24 +1,24 @@
 import unittest
-from api import api
+from api import app
 
 class ApiTest(unittest.TestCase):
 
    def setUp(self):
-      self.app = api.app.test_client()
+      self.app = app.app.test_client()
 
    def test_base_api_url(self):
-      self.assertEqual( api.url_for_endpoint('pippo') , '/api/v1.0/pippo/' )
+      self.assertEqual( app.url_for_endpoint('pippo') , '/api/v1.0/pippo/' )
 
    def test_base_api_url_with_params(self):
-      self.assertEqual( api.url_for_endpoint('pippo/<float:age>') , '/api/v1.0/pippo/<float:age>/' )
+      self.assertEqual( app.url_for_endpoint('pippo/<float:age>') , '/api/v1.0/pippo/<float:age>/' )
 
    def test_buildings_uri(self):
       response = self.app.get('/api/v1.0/buildings/')
       self.assertEqual(response.status_code,200)
 
    def test_buildings_by_id_with_valid_bid(self):
-      response = self.app.get('/api/v1.0/buildings/1245/')
-      self.assertEqual(response.status_code,200)
+      response = self.app.get('/api/v1.0/buildings/aaaa/')
+      self.assertEqual(response.status_code,400)
 
    def test_buildings_by_id_with_invalid_bid(self):
       response = self.app.get('/api/v1.0/buildings/pippo/')
