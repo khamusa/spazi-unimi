1546e4e9dada3584a59b0fe5ab5d267062921a82
Diego Costantino <diegocstn@gmail.com>
2015-09-13 15:14:45 +0200

    AvailableServiceDataUpdater class doc

--- tasks/data_updaters/available_services_data_updater.py
+++ tasks/data_updaters/available_services_data_updater.py
@@ -1,6 +1,10 @@
 from model import AvailableService
 
 class AvailableServicesDataUpdater:
+   """
+   The AvailableServicesDataUpdater class implements the interface
+   for query the available services used for filter buildings and floors
+   """
 
    def perform_update(self,entities_type, content):
       AvailableService.clean()
